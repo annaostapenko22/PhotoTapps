eca70366c580d8789f48c00c030813e947bec839
Anna Ostapenko <anna.ostapenko272@gmail.com>
2021-03-10 13:45:50 +0100

    Initial Commit

---
+++ PhotoTapps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoTapps
+//
+//  Created by pc on 3/10/21.
+//  Copyright © 2021 pc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
